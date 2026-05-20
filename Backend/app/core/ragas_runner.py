ea901065cd19bc76053b2305f45abcbbaa0d6b8c
Tannu Tiwari <tannutiwari9953@gmail.com>
2026-05-20 22:53:24 +0530

    new files

--- Backend/app/core/ragas_runner.py
+++ Backend/app/core/ragas_runner.py
@@ -6,14 +6,15 @@ from ragas.metrics import (
     context_recall,
     answer_correctness
 )
-
 async def run_ragas(question, answer, contexts, ground_truth):
+    
     dataset = Dataset.from_dict({
         "question": [question],
         "answer": [answer],
         "contexts": [contexts],
         "ground_truth": [ground_truth]
     })
+
     result = evaluate(dataset, metrics=[
         faithfulness, answer_relevancy,
         context_precision, context_recall,
